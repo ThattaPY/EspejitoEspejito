76814f20282a81ea1d5ae313942af881e241922e
ThattaPy <thattasystems@gmail.com>
2023-07-23 10:30:47 +0200

    ViewModel instanciate

--- app/src/main/java/com/thatta/espejitoespejito/views/MainActivity.kt
+++ app/src/main/java/com/thatta/espejitoespejito/views/MainActivity.kt
@@ -2,18 +2,20 @@ package com.thatta.espejitoespejito.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.thatta.espejitoespejito.R
 import com.thatta.espejitoespejito.databinding.ActivityMainBinding
+import com.thatta.espejitoespejito.viewModels.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private val viewModel by viewModels<MainActivityViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater, null, false)
         setContentView(binding.root)
-
+        viewModel
 
     }
 }
